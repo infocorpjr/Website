2423749e1c91e7d164ff5ab318568a429419bd15
Jorge Rodrigues <jorgerodrigues9@outlook.com>
2018-09-08 11:28:29 -0400

    Adicionado seção de serviços.

--- src/js/app.js
+++ src/js/app.js
@@ -17,3 +17,34 @@ window.Vue = require('vue');
 const app = new Vue({
     el: '#app'
 });
+
+/**
+ * Swiper 4.3.3
+ * Most modern mobile touch slider and framework with hardware accelerated transitions
+ * http://www.idangero.us/swiper/
+ *
+ * Copyright 2014-2018 Vladimir Kharlampidi
+ *
+ * Released under the MIT License
+ *
+ * Released on: June 5, 2018
+ */
+import Swiper from 'swiper';
+
+const Services = new Swiper('.swiper-container', {
+    autoplay: {
+        delay: 3000,
+    },
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is <= 480px
+        991: {
+            slidesPerView: 1,
+        },
+    }
+});
